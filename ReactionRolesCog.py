aaab4ea0dc07a0cd8a18ef7c0a7f9bbb9ad94bfc
Erik Mekkes <Mekkes.Erik@gmail.com>
2020-09-07 20:04:55 +0200

    Update references

--- ReactionRolesCog.py
+++ ReactionRolesCog.py
@@ -1,6 +1,6 @@
+import zerobot_common
 from discord.ext import commands
 from rankchecks import discord_ranks
-from zerobot_common import guild, reaction_messages, reactionlog, discord_roles
 
 class ReactionRolesCog(commands.Cog):
     '''
@@ -8,7 +8,7 @@ class ReactionRolesCog(commands.Cog):
     '''
     def __init__(self, bot):
         self.bot = bot
-        reactionlog.log(f'Reactionroles cog loaded and ready.')
+        zerobot_common.reactionlog.log(f'Reactionroles cog loaded and ready.')
     
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload):
@@ -17,7 +17,7 @@ class ReactionRolesCog(commands.Cog):
         Should try very hard to keep this efficient, return asap if not relevant.
         '''
         # check if message should grant roles, return if not a reaction role message.
-        reaction_message = reaction_messages.get(str(payload.message_id))
+        reaction_message = zerobot_common.reaction_messages.get(str(payload.message_id))
         if (reaction_message == None):
             return
         # find role id to give for that reaction to that message, return if no role to give
@@ -25,17 +25,17 @@ class ReactionRolesCog(commands.Cog):
         if (role_id == None):
             return
         # find discord user, return if can not load the discord user to modify roles for
-        discord_user = guild.get_member(payload.user_id)
+        discord_user = zerobot_common.guild.get_member(payload.user_id)
         if (discord_user == None):
-            reactionlog.log(f'Could not find user : {payload.user_id} on {guild.name}')
+            zerobot_common.reactionlog.log(f'Could not find user : {payload.user_id} on {zerobot_common.guild.name}')
             return
         # return if role to add cant be found
-        role_to_add = guild.get_role(role_id)
+        role_to_add = zerobot_common.guild.get_role(role_id)
         if (role_to_add == None):
-            reactionlog.log(f'Could not find role : {role_id} on {guild.name}')
+            zerobot_common.reactionlog.log(f'Could not find role : {role_id} on {zerobot_common.guild.name}')
             return
         # specific case for waiting approval role
-        if (role_id == discord_roles.get('Waiting Approval')):
+        if (role_id == zerobot_common.discord_roles.get('Waiting Approval')):
             # return if user already has a ranked role, dont need to assign waiting approval.
             for role in discord_user.roles:
                 if (role.name in discord_ranks):
